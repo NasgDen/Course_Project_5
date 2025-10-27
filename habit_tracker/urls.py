909484587e49387edf343a6f9e814024c6cd2c44
Denis Safonov <denissafonov_85@mail.ru>
2025-10-27 20:27:19 +0500

    Реализован CRUD для модели Habit - Привычка.

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from .apps import HabitTrackerConfig
-from .views import HabitCreateApiView, HabitListApiView
+from .views import HabitCreateApiView, HabitListApiView, HabitUpdateApiView, HabitDeleteAPIView
 
 app_name = HabitTrackerConfig.name
 
 urlpatterns = [
     path("create/", HabitCreateApiView.as_view(), name="habit_create"),
     path("list/", HabitListApiView.as_view(), name="habit_list"),
+    path("update/<int:pk>/", HabitUpdateApiView.as_view(), name="habit_update"),
+    path("retrieve/<int:pk>/", HabitUpdateApiView.as_view(), name="habit_retrieve"),
+    path("delete/<int:pk>/", HabitDeleteAPIView.as_view(), name="habit_delete"),
 ]
--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -6,10 +6,35 @@ from habit_tracker.serializers import HabitSerializer
 
 
 class HabitCreateApiView(generics.CreateAPIView):
+    """Класс реализует интерфейс для создания данных о привычки"""
+
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
 
 
 class HabitListApiView(generics.ListAPIView):
+    """Класс реализует интерфейс для вывода всех привычек"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitUpdateApiView(generics.UpdateAPIView):
+    """Класс реализует интерфейс для изменения данных о привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """Класс реализует интерфейс для отображения данных об одной привычке"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDeleteAPIView(generics.DestroyAPIView):
+    """Класс реализует интерфейс для удаления данных о привычки"""
+
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
