20834774dfd141caa0b3138124e4c4d7940e04b1
Denis Safonov <denissafonov_85@mail.ru>
2025-10-27 19:51:10 +0500

    Создана миграция моделей

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     )
     city = models.CharField(max_length=50, verbose_name="Город", help_text="Введите город", blank=True, null=True)
     avatar = models.ImageField(upload_to="users/avatar/", blank=True, null=True)
-    tg_token = models.CharField(max_length=150, verbose_name="Токен телеграмм", help_text="Введите токен пользователя в телеграм")
+    tg_token = models.CharField(max_length=150, verbose_name="Токен телеграмм", help_text="Введите токен пользователя в телеграм", blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
