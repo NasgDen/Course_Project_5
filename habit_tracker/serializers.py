7b48e1212d1789460ea717a01a0875b6707fa37e
Denis Safonov <denissafonov_85@mail.ru>
2025-10-28 20:55:52 +0500

    Реализована валидация поля periodicity модели Habit

--- habit_tracker/serializers.py
+++ habit_tracker/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
 from habit_tracker.models import Habit
-from habit_tracker.validators import TimeValidator
+from habit_tracker.validators import TimeValidator, PeriodicityValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
-        validators = [TimeValidator(field="time_complete")]
+        validators = [TimeValidator(field="time_complete"), PeriodicityValidator(field="periodicity")]
--- habit_tracker/validators.py
+++ habit_tracker/validators.py
@@ -9,7 +9,18 @@ class TimeValidator:
         self.field = field
 
     def __call__(self, value):
-        print("VALUE:", dict(value).get(self.field))
         if dict(value).get(self.field) > timedelta(seconds=120):
             raise ValidationError("Время выполнения должно быть не больше 120 секунд.")
+
+
+class PeriodicityValidator:
+    """Класс реализует валидацию поля periodicity модели Habit"""
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        if dict(value).get(self.field) > 7:
+            raise ValidationError("Нельзя выполнять привычку реже, чем 1 раз в 7 дней.")
+
 
